a690134a4782e20c650dedad3ab4208caa57cf54
Sharma3850 <yogesh.sharma3850@gmail.com>
2024-05-16 21:00:30 +0530

    adding endpoints and templates

--- index.js
+++ index.js
@@ -1,10 +1,20 @@
 require('dotenv').config()
 const express = require('express')
+const path = require('path')
 const app = express()
 const port = 3000
 
+const obja = [
+name="Yogesh"
+]
+app.get('/:name', (req, res) => {
+    // res.send('Hello World!')
+    res.send("Hello " + req.params.name)
+})
+
 app.get('/', (req, res) => {
-    res.send('Hello World!')
+    // res.send('Hello World!')
+    res.sendFile(path.join(__dirname,'./templates/index.html'))
 })
 
 app.get('/login', (req, res) => {
@@ -15,6 +25,10 @@ app.get('/signup', (req, res) => {
 })
 app.get('/chai', (req, res) => {
     res.send('<h2>Chai!</h2>')
+})
+
+app.get('/api', (req, res) => {
+    res.json({obja})
 })
 
 app.listen(process.env.PORT, () => {
